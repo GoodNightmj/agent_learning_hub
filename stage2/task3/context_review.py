8277b8cc81587e7e804f7e7e2cbe359370398bba
Mj <m13383250308@gmail.com>
2026-08-13 21:22:33 +0800

    context_review

---
+++ stage2/task3/context_review.py
@@ -0,0 +1,39 @@
+def plan_compression(
+    messages: list,
+    keep_recent_turns: int
+) -> dict:
+    compressed_messages = []
+    turns=[]
+    current_turn = []
+    for message in messages:
+        if message["role"] == "system":
+            continue
+        elif message["role"] == "user":
+            if current_turn:
+                turns.append(current_turn)
+            current_turn = [message]
+        else:
+            current_turn.append(message)
+        if current_turn:
+            turns.append(current_turn)
+    system_message = messages[0]
+    if len(turns) <= keep_recent_turns:
+        return{
+            "system_message": system_message,
+            "old_turns": "",
+            "recent_turns": turns,
+            "messages_to_summarize": [],
+            "recent_messages": [].extend([msg for turn in turns for msg in turn])
+        }
+    else:
+        old_turns = turns[:-keep_recent_turns]
+        recent_turns = turns[-keep_recent_turns:]
+        messages_to_summarize = [msg for turn in old_turns for msg in turn]
+        recent_messages = [msg for turn in recent_turns for msg in turn]
+        return {
+            "system_message": system_message,
+            "old_turns": old_turns,
+            "recent_turns": recent_turns,
+            "messages_to_summarize": messages_to_summarize,
+            "recent_messages": recent_messages
+        }
